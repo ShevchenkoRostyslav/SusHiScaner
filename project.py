a0a180ee8d413e1b83d9172b23f67e7319851686
Rostyslav Shevchenko <shevchenko.rostislav@gmail.com>
2016-11-20 18:15:48 +0100

    Clean unused libs

--- project.py
+++ project.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 import logging
-import sys
-import os
-sys.path.insert(0,'../project')
 from lib.parseInput import *
 from lib.submitter import *
 
@@ -15,8 +12,3 @@ if __name__ == '__main__':
     # Start submitting the jobs
     proc = submitter.choose_cluster(cmd_args.submitter)
     proc.submit(cmd_args,in_args)
-
-
-
-
-    # if not len(sys.argv) > 1: logging.info('no arguments were provided')
