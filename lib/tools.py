cca01a24d38df84492d8939aaff2648700ae1a8d
Rostyslav Shevchenko <shevchenko.rostislav@gmail.com>
2016-11-24 19:11:23 +0100

    New tool to make file executable(chmod)

--- lib/tools.py
+++ lib/tools.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
+
+"""Module with usefull tools
+
+"""
+
 import os
 import shutil # for rmtree function
+import stat
+
+__author__ = "Rostyslav Shevchenko"
+__email__ = "rostyslav.shevchenko@desy.de"
 
 # Get absolute path to the file/dir
 def getAbsPath(path_to_file):
@@ -14,3 +23,7 @@ def MakeCleanDir(path_to_dir):
     os.makedirs(path_to_dir)
     # return the path
     return path_to_dir
+
+def MakeFileExecutable(path_to_file):
+    st = os.stat(path_to_file)
+    os.chmod(path_to_file, st.st_mode | stat.S_IEXEC)
