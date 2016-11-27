fb543adfa2341663d720dc7a7ea7a5a52aa42475
Rostyslav Shevchenko <shevchenko.rostislav@gmail.com>
2016-11-27 19:10:43 +0100

    Postprocessor. Remove root name from input args

--- postprocessor.py
+++ postprocessor.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 import argparse
 from lib.tools import *
-from lib.makeReadableGrid import *
+from lib.getInfoFromSusHiOutput import *
 from lib.CheckJobs import *
 
 # read output of thw submission --> input for post-processing
 def ParseOption():
     parser = argparse.ArgumentParser(description='Convert the output of the sushi+2HDMC to .root + .txt format')
     parser.add_argument('-i',       dest='input_dir', type=str, help='path with output of the submission tool')
-    parser.add_argument('-o',       dest='output',    type=str, help='output root file name',default='output')
     args = parser.parse_args()
     # convert input to absolute path
     args.input_dir  = getAbsPath(args.input_dir) + '/'
@@ -19,4 +18,5 @@ if __name__ == '__main__':
     cmd_args = ParseOption()
     # First - check jobs:
     CheckSubmissionOutput(cmd_args.input_dir + 'sushi_out/')
+    # make .txt and .root files
     makeReadableGrid(cmd_args.input_dir)
